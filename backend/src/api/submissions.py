65e6681cd8402dd4d5166b700dbbb94e07c669c0
Mohamed Alassal <mohmed.alassal99@gmail.com>
2026-05-16 20:23:19 +0300

    Refactor submissions API endpoints to include user-specific data and improve clarity

--- backend/src/api/submissions.py
+++ backend/src/api/submissions.py
@@ -9,21 +9,21 @@ from src.database import SessionDep, create_db_and_tables
 from src.core.security import verify_access_token, get_current_user
 router = APIRouter()
 
-@router.get("/submissions",dependencies=[Depends(verify_access_token)]) #pyright: ignore
-def read_submissions(session: SessionDep, status: Annotated[SubmissionStatus | None, Query()] = None): #pyright: ignore
-    statement = select(Submission)
+@router.get("/my-submissions",dependencies=[Depends(verify_access_token)]) #pyright: ignore
+def read_submissions(session: SessionDep,current_user: Annotated[User, Depends(get_current_user)], status: Annotated[SubmissionStatus | None, Query()] = None): #pyright: ignore
+    statement = select(Submission).where(Submission.user_id == current_user.id) #pyright: ignore
     if status is not None:
         statement = statement.where(Submission.status == status)
     statement = statement.order_by(Submission.id) #pyright:ignore
     submissions = session.exec(statement).all()
     return submissions
 
-@router.post("/submissions", status_code=201,dependencies=[Depends(verify_access_token)]) #pyright: ignore
+@router.post("/submit", status_code=201,dependencies=[Depends(verify_access_token)]) #pyright: ignore
 async def create_submission(submission: Annotated[SubmissionCreate, Body(embed=False)], session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):
     if(not session.exec(select(Problem).where(Problem.id == submission.problem_id)).first()): #pyright: ignore
             raise HTTPException(status_code=404, detail="Problem not found")
     submission_db = Submission(
-        code=submission.code,
+        code=submission.source_code,
         problem_id=submission.problem_id,
         user_id=current_user.id)  #pyright: ignore
     session.add(submission_db)  #pyright: ignore
@@ -31,9 +31,9 @@ async def create_submission(submission: Annotated[SubmissionCreate, Body(embed=F
     session.refresh(submission_db)  #pyright: ignore
     return submission_db
 
-@router.get("/submissions/{submission_id}",dependencies=[Depends(verify_access_token)]) #pyright: ignore
-def read_submission(submission_id: int, session: SessionDep): #pyright: ignore
-    submission = session.exec(select(Submission).where(Submission.id == submission_id)).first()#pyright: ignore
+@router.get("/my-submissions/{submission_id}",dependencies=[Depends(verify_access_token)]) #pyright: ignore
+def read_submission(submission_id: int, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]): #pyright: ignore
+    submission = session.exec(select(Submission).where(Submission.id == submission_id, Submission.user_id == current_user.id)).first()#pyright: ignore
     if not submission:
         raise HTTPException(status_code=404, detail="Submission not found")
     return submission

2663b660ed27a628a27c452459613a28db61b81c
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-05-19 23:27:25 +0300

    Remove index from verdict field in submission model

--- backend/src/models/submission.py
+++ backend/src/models/submission.py
@@ -19,4 +19,4 @@ class Submission(SQLModel, table=True):
     submitted_at: datetime = Field(default_factory=utc_now)
     execution_time: int | None = None
     execution_memory: int | None = None 
-    verdict: SubmissionStatus | None = Field(default=None, index=True)
+    verdict: SubmissionStatus | None = Field(default=None)

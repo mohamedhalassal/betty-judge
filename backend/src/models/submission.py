a63b25a8181440bde12a6fc9f9d574e3a4a63fb0
abdo-saad <abdosaadasd5513@gmail.com>
2026-05-21 20:33:05 +0300

    update submission model

--- backend/src/models/submission.py
+++ backend/src/models/submission.py
@@ -6,6 +6,7 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 def utc_now(): return datetime.now(timezone.utc)
 
 class SubmissionStatus(str, Enum):
+    IN_QUEUE = "in queue"
     ACCEPTED = "accepted"
     WRONG_ANSWER = "wrong_answer"
     TIME_LIMIT_EXCEEDED = "time_limit_exceeded"
@@ -16,10 +17,11 @@ class SubmissionStatus(str, Enum):
 class Submission(SQLModel, table=True):
     __tablename__ = "submissions"
     id:int |None = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="users.id",index=True)
-    problem_id: int = Field(foreign_key="problems.id", index=True)
+    user_id: int = Field(foreign_key="users.id")
+    problem_id: int = Field(foreign_key="problems.id")
     source_code: str
-    submitted_at: datetime = Field(default_factory=utc_now) 
+    submitted_at: datetime = Field(default_factory=utc_now)
     execution_time: int | None = None
     execution_memory: int | None = None 
     status: SubmissionStatus | None = Field(default=None)
+

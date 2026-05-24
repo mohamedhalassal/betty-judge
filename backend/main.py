0bfc6a8457801a15530304696adbc0080073e206
abdo-saad <abdosaadasd5513@gmail.com>
2026-05-25 00:02:12 +0300

    update imports in main

--- backend/main.py
+++ backend/main.py
@@ -12,13 +12,11 @@ from src.api.auth import router as auth_router
 from src.api.problems import router as problems_router
 from src.api.test_cases import router as test_cases_router
 from src.api.submissions import router as submissions_router
-from src.api.get_problem_from_polygon import router as polygon_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(problems_router)
 app.include_router(test_cases_router)
 app.include_router(submissions_router)
-app.include_router(polygon_router)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()

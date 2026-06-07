65ead16de2c94fd69f4a74a447f7b0bc75807e81
Mohamed Alassal <mohmed.alassal99@gmail.com>
2026-06-07 20:10:23 +0300

    Refactor queue client initialization into a separate function

--- backend/src/services/queue_service.py
+++ backend/src/services/queue_service.py
@@ -2,11 +2,13 @@ import os
 from azure.storage.queue import QueueClient
 import json
 
-queue_client = QueueClient.from_connection_string(
-    conn_str=os.getenv("AZURE_QUEUE_CONNECTION_STRING"),
-    queue_name=os.getenv("AZURE_QUEUE_NAME")
-)
+def get_queue_client():
+     return QueueClient.from_connection_string(
+        conn_str=os.getenv("AZURE_QUEUE_CONNECTION_STRING"),
+        queue_name=os.getenv("AZURE_QUEUE_NAME")
+    )
 
 def send_submission(submission_id: int):
     message = json.dumps({"submission_id": submission_id})
+    queue_client = get_queue_client()
     queue_client.send_message(message)

e3fa68342926e7ab4481d4c634e6d2a3d76d3e1c
Mohamed Alassal <mohmed.alassal99@gmail.com>
2026-06-09 22:14:35 +0300

    Refactor submission handling to use dependency injection for queue service and implement message queue service

--- backend/src/services/queue_service.py
+++ backend/src/services/queue_service.py
@@ -1,14 +1,15 @@
 import os
-from azure.storage.queue import QueueClient
 import json
+from azure.storage.queue import QueueClient
+from src.services.message_queue import MessageQueue
 
-def get_queue_client():
-     return QueueClient.from_connection_string(
-        conn_str=os.getenv("AZURE_QUEUE_CONNECTION_STRING"),
-        queue_name=os.getenv("AZURE_QUEUE_NAME")
-    )
 
-def send_submission(submission_id: int):
-    message = json.dumps({"submission_id": submission_id})
-    queue_client = get_queue_client()
-    queue_client.send_message(message)
+class AzureQueueService(MessageQueue):
+    def __init__(self):
+        self.client = QueueClient.from_connection_string(
+            conn_str=os.getenv("AZURE_QUEUE_CONNECTION_STRING"),
+            queue_name=os.getenv("AZURE_QUEUE_NAME")
+        )
+    def send_submission(self, submission_id: int):
+        message = json.dumps({"submission_id": submission_id})
+        self.client.send_message(message)

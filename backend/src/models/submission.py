4965f380c7f1f3e1be4b14aa033419474b0844a0
abdo-saad <abdosaadasd5513@gmail.com>
2026-06-10 19:46:20 +0300

    update submissionStatues enum

--- backend/src/models/submission.py
+++ backend/src/models/submission.py
@@ -14,6 +14,7 @@ class SubmissionStatus(str, Enum):
     COMPILE_ERROR = "compile error"
     MEMORY_LIMIT_EXCEEDED = "memory limit exceeded"
     IDLENESS_LIMIT_EXCEEDED = "idleness limit exceeded"
+    FAILED = "failed"
 
 class Submission(SQLModel, table=True):
     __tablename__ = "submissions"

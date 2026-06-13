4889a55896afbca50f7f6762a0bca616a8ced4cc
Mohamed Alassal <mohmed.alassal99@gmail.com>
2026-06-13 05:11:49 +0300

    Refactor sync_submissions function to solve N+1 Problem

--- test_schema/click_CLI/submissions_sync_CLI.py
+++ test_schema/click_CLI/submissions_sync_CLI.py
@@ -5,22 +5,21 @@ from models.problem import Problem
 from models.submission import Submission
 import click
 
-
 engine = create_engine(os.getenv("DATABASE_URL"))
+
 
 def sync_submissions():
     with Session(engine) as session:
-        submissions = session.exec(
-            select(Submission).where(Submission.revision_id == None)
-        ).all()
-        for submission in submissions:
-            problem = session.get(Problem, submission.problem_id)
-            if problem is None:
-                print(f"Problem with id {submission.problem_id} not found for submission {submission.id}")
-                continue
-            if problem.revision_id is None:
-                print(f"Problem with id {problem.id} has not been synced yet for submission {submission.id}")
-                continue
+        statement = (
+            select(Submission, Problem)
+            .join(Problem, Submission.problem_id == Problem.id)
+            .where(
+                Submission.revision_id == None,
+                Problem.revision_id != None,
+            )
+        )
+        results = session.exec(statement).all()
+        for submission,problem in results:
             try:
                 backend_submission = create_submission(
                     problem.revision_id, submission.source_code
@@ -35,9 +34,10 @@ def sync_submissions():
 
 @click.command()
 def main():
-	click.echo("Syncing submissions...")
-	sync_submissions()
-	click.echo("Submissions sync finished.")
+    click.echo("Syncing submissions...")
+    sync_submissions()
+    click.echo("Submissions sync finished.")
+
 
 if __name__ == "__main__":
     main()
